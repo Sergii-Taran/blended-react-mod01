46d9eaf95147a0e77e29c8ddf2153542ae24bec3
Sergii Taran <serg.taran1970@gmail.com>
2026-02-17 13:44:52 +0200

    Initial commit

---
+++ src/task-6.ts
@@ -0,0 +1,24 @@
+// Задача 6. Узагальнені типи
+// Функція getFirstElement приймає масив і повертає його перший елемент.
+
+// function getFirstElement(arr) {
+//   return arr[0];
+// }
+
+// getFirstElement([1, 2, 3]);
+// getFirstElement(["a", "b", "c"]);
+// getFirstElement([true, false, true]);
+
+// Завдання:
+// Зроби функцію узагальненою, використовуючи тип T, щоб вона працювала з масивами будь-якого типу.
+// Додай явну типізацію дженериків у виклики функцій.
+// Переконайся, що тип елемента, який повертається, точно відповідає типу елементів у масиві.
+// Перевір, що TypeScript не дозволяє передати масив змішаних типів без відповідного типу.
+
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]);
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false, true]);
