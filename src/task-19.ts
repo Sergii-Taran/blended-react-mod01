3825562b6422f810b6d2fc1e651fbd2b4dd891a2
Sergii Taran <serg.taran1970@gmail.com>
2026-02-20 08:50:41 +0200

    feat: type fetchUser with axios generic and User interface (task 19)

---
+++ src/task-19.ts
@@ -0,0 +1,66 @@
+// Задача 19
+// Функція fetchUser повертає проміс, який через axios отримує одного користувача з API по userId.
+
+// import axios from "axios";
+
+// const fetchUser = async (userId) => {
+//   const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+//   return response.data;
+// };
+
+// const getUserName = async (id) => {
+//   const user = await fetchUser(id);
+//   console.log(user.name);
+// };
+
+// getUserName(1);
+
+// Завдання:
+// Оголосіть інтерфейс User для користувача (перевірте, які властивості користувача містяться у відповіді бекенда).
+// Типізуйте функцію fetchUser.
+
+import axios from "axios";
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+const fetchUser = async (userId: number): Promise<User> => {
+  const response = await axios.get<User>(
+    `https://jsonplaceholder.typicode.com/users/${userId}`,
+  );
+  return response.data;
+};
+
+const getUserName = async (id: number): Promise<void> => {
+  const user = await fetchUser(id);
+  console.log(user.name);
+};
+
+getUserName(1);
